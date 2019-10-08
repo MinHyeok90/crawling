654aa245dacb32f11383cfab725abd93833cb0b5
MinHyuk <dufflaa@gmail.com>
2019-10-08 21:09:22 +0900

    add Todos for development, database and save result

--- crawling.py
+++ crawling.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
+import datetime
 import requests
+import pandas
 import json
 import os
 
@@ -9,6 +11,20 @@ file_title = 'result.json'
 search_url = 'http://api.encar.com/search/car/list/premium?count=true&q=(And.Hidden.N._.(C.CarType.Y._.Manufacturer.%ED%98%84%EB%8C%80.)_.OfficeCityState.%EA%B2%BD%EA%B8%B0._.Trust.ExtendWarranty._.Options.%EB%B8%8C%EB%A0%88%EC%9D%B4%ED%81%AC+%EC%9E%A0%EA%B9%80+%EB%B0%A9%EC%A7%80(ABS_)._.Options.%ED%9B%84%EB%B0%A9+%EC%B9%B4%EB%A9%94%EB%9D%BC._.Options.%EC%A3%BC%EC%B0%A8%EA%B0%90%EC%A7%80%EC%84%BC%EC%84%9C(%EC%A0%84%EB%B0%A9_)._.Category.SUV.)&sr=%7CModifiedDate%7C0%7C100'
 item_url_pre = 'http://www.encar.com/dc/dc_cardetailview.do?pageid=dc_carsearch&listAdvType=normal&carid='
 item_url_post = '&wtClick_korList=019&advClickPosition=kor_normal_p1_g1'
+
+test_mode = True
+test_limit_cnt = 3
+
+
+def is_test_mode():
+    return test_mode
+
+
+def limiter_is_nedded_limit_for_test():
+    if is_test_mode:
+        return True
+    else:
+        return False
 
 
 def save_data_title(data, title):
@@ -142,13 +158,34 @@ def get_additional_info_from_url_by_records(table):
     return table
 
 
+def create_database():
+    # TODO: use sqlite3
+    print("not impl")
+
+
+def get_added_ids(prev_ids, cur_ids):
+    # TODO: 
+    print("not impl")
+
+
+def get_removed_ids(prev_ids, cur_ids):
+    # TODO: 
+    print("not impl")
+
+
+def save_to_csv(json_data):
+    df = pandas.read_json(json_data)
+    df.to_csv("test.csv", encoding='utf-8')
+
+
 def main():
     # json_data = crawling_and_save()
     json_data = from_file()
     result_records = extract_my_interest(json_data)
     result_table = get_additional_info_from_url_by_records(result_records)
     print_my_interests(my_interest_order_and_photodate_view(), result_table)
-    save_data_title(result_table, "result.json")
+    save_data_title(result_table, "result_" + str(datetime.datetime.now()) + ".json")
+    save_to_csv(result_table)
 
 
 main()
