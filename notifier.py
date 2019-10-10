22f5355a78386a4c265626f3f504a1c3b3790861
MinHyuk <dufflaa@gmail.com>
2019-10-11 04:08:16 +0900

    bug fix : too many message : limit 20msg/60sec

--- notifier.py
+++ notifier.py
@@ -16,34 +16,29 @@ def get_chat_room():
     return chat_room_number
 
 
-# 생성한 텔레그램 봇 /start 시작 후 사용자 id 받아 오기
-# chat_id = bot.getUpdates()[-1].message.chat.id
-# print('user id :', chat_id)
-# 사용자 id로 메시지 보내기
-# bot.sendMessage(chat_id, u'bot이 보낸 메시지')
-
 def send(bot, chat_room, message):
     bot.sendMessage(chat_room, message)
+    # print(message)
 
 
 def create_link(num):
-    url_pre = u'www.encar.com/dc/dc_cardetailview.do?pageid=dc_carsearch&listAdvType=normal&carid='
-    url_post = u'&wtClick_korList=019&advClickPosition=kor_normal_p1_g1'
+    url_pre = 'www.encar.com/dc/dc_cardetailview.do?pageid=dc_carsearch&listAdvType=normal&carid='
+    url_post = '&wtClick_korList=019&advClickPosition=kor_normal_p1_g1'
     return url_pre + num + url_post
 
 
 def new_car_msg(newer):
     hello = "===신규 매물! " + str(len(newer)) + "건\n"
-    contents = ""
+    contents_list = []
     for car in newer:
         id = car['Id']
-        contents = contents + \
-                   "\n번호: " + id + \
+        contents = "번호: " + id + \
                    "\n차종: " + car['Model'] + \
                    "\n가격: " + str(car['Price']) + \
                    "\n링크: " + create_link(id) + \
                    "\n"
-    return hello + contents
+        contents_list.append(contents)
+    return hello, contents_list
 
 
 def leave_car_msg(leave):
@@ -56,23 +51,40 @@ def deleted_car_msg(deleted):
     return deleted_contents
 
 
-def converter_readability(separated_by_status):
+def notify_newer_cars(separated_by_status):
+    bot = get_bot()
+    chat_room = get_chat_room()
     newer = separated_by_status['newer']
+    hello, contents = new_car_msg(newer)
+    send(bot, chat_room, hello)
+    if len(contents) > 15:
+        for i in range(15):
+            send(bot, chat_room, str(i) + "번째 매물\n" + contents[i])
+    else:
+        for c in contents:
+            send(bot, chat_room, c)
+
+
+def notify_leave_cars(separated_by_status):
+    bot = get_bot()
+    chat_room = get_chat_room()
     leave = separated_by_status['leave']
+    readable_data = leave_car_msg(leave)
+    send(bot, chat_room, readable_data)
+
+
+def notify_deleted_cars(separated_by_status):
+    bot = get_bot()
+    chat_room = get_chat_room()
     deleted = separated_by_status['deleted']
-
-    ncm = new_car_msg(newer)
-    lcm = leave_car_msg(leave)
-    dcm = deleted_car_msg(deleted)
-
-    return ncm + "\n" + lcm + "\n" + dcm
+    readable_data = deleted_car_msg(deleted)
+    send(bot, chat_room, readable_data)
 
 
 def notify(separated_by_status):
-    bot = get_bot()
-    chat_room = get_chat_room()
-    readable_data = converter_readability(separated_by_status)
-    send(bot, chat_room, readable_data)
+    notify_newer_cars(separated_by_status)
+    notify_leave_cars(separated_by_status)
+    notify_deleted_cars(separated_by_status)
 
 
 def test():
@@ -82,7 +94,6 @@ def test():
 
     # print("converter_readability test")
     data = test.get_separated_by_status_three_newer()
-    x = converter_readability(data)
     # print(x)
 
     print("send long message test")
