0969697ccc2a06bed1cc6968ec7272bbf6ea2d23
MinHyuk <dufflaa@gmail.com>
2019-10-11 01:56:44 +0900

    implement app_repo

--- app_repository.py
+++ app_repository.py
@@ -5,31 +5,10 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 myclient = pymongo.MongoClient("mongodb://192.168.99.100:27017/")
 
 
-def save_all(list_data):
-    mydb = myclient.jungo_car_app
-    mycol = mydb.jungo_cars
-    x = mycol.insert_many(list_data)
-
-
-def find_all():
-    mydb = myclient.jungo_car_app
-    mycol = mydb.jungo_cars
-    res = []
-    for x in mycol.find():
-        res.append(x)
-    return res
-
-
-def drop_all():
-    mydb = myclient.jungo_car_app
-    mycol = mydb.jungo_cars
-    mycol.drop()
-
-
-def save_leave_cars():
+def save_leave_cars(leave_cars):
     mydb = myclient.jungo_car_app
     mycol = mydb.leave_cars
-    mycol.drop()
+    mycol.insert_many(leave_cars)
 
 
 def load_leave_cars():
@@ -41,10 +20,16 @@ def load_leave_cars():
     return res
 
 
-def save_deleted_cars():
+def drop_leave_cars():
+    mydb = myclient.jungo_car_app
+    mycol = mydb.leave_cars
+    mycol.drop()
+
+
+def save_deleted_cars(deleted_cars):
     mydb = myclient.jungo_car_app
     mycol = mydb.deleted_cars
-    mycol.drop()
+    mycol.insert_many(deleted_cars)
 
 
 def load_deleted_cars():
@@ -56,27 +41,27 @@ def load_deleted_cars():
     return res
 
 
-def save_all_test():
-    mylist = [
-        {'Id': '25167131', 'ModifiedDate': '2019-10-09 23:57:38.000 +09', 'Manufacturer': '현대', 'Price': 1950.0,
-         'Year': 201402.0, 'Mileage': 79786.0, 'Model': '맥스크루즈', 'Badge': '디젤(e-VGT) 2.2 4WD 익스클루시브', '조회수': '200',
-         '찜수': '2'},
-        {'Id': '25479305', 'ModifiedDate': '2019-10-09 23:56:59.000 +09', 'Manufacturer': '현대', 'Price': 2270.0,
-         'Year': 201708.0, 'Mileage': 8098.0, 'Model': '코나', 'Badge': '1.6 터보 4WD', '조회수': '720', '찜수': '6'},
-        {'Id': '25109309', 'ModifiedDate': '2019-10-09 23:53:59.000 +09', 'Manufacturer': '현대', 'Price': 1750.0,
-         'Year': 201302.0, 'Mileage': 33488.0, 'Model': '싼타페 DM', 'Badge': '디젤(e-VGT) 2.0 2WD 프리미엄', '조회수': '933',
-         '찜수': '9'},
-        {'Id': '25377212', 'ModifiedDate': '2019-10-01 20:18:39.000 +09', 'Manufacturer': '현대', 'Price': 2590.0,
-         'Year': 201705.0, 'Mileage': 67541.0, 'Model': '더 뉴 맥스크루즈', 'Badge': '디젤 2.2 4WD', '조회수': '1042', '찜수': '3'},
-    ]
-    save_all(mylist)
+def drop_deleted_cars():
+    mydb = myclient.jungo_car_app
+    mycol = mydb.deleted_cars
+    mycol.drop()
 
 
-# save_all_test()
+def drop_all():
+    drop_leave_cars()
+    drop_deleted_cars();
 
 
-def find_all_test():
-    x = find_all()
-    for i in x:
-        print(x)
-# find_all_test()
+def update_leave_and_deleted(leave_and_deleted):
+    leave = leave_and_deleted['leave']
+    deleted = leave_and_deleted['deleted']
+    drop_leave_cars()
+    save_leave_cars(leave)
+    save_deleted_cars(deleted)
+
+
+def test():
+    import test
+    print("update_leave_and_deleted test")
+    d = test.get_separated_by_status()
+    update_leave_and_deleted(d)
--- test.py
+++ test.py
@@ -12,6 +12,7 @@ def get_test_list_original():
          'Year': 201705.0, 'Mileage': 67541.0, 'Model': '더 뉴 맥스크루즈', 'Badge': '디젤 2.2 4WD', '조회수': '1042', '찜수': '3'},
     ]
 
+
 def get_test_list_newer():
     return [
         {'Id': '99999999', 'ModifiedDate': '2019-10-09 23:57:38.000 +09', 'Manufacturer': '현대', 'Price': 1950.0,
@@ -25,3 +26,25 @@ def get_test_list_newer():
         # {'Id': '25377212', 'ModifiedDate': '2019-10-01 20:18:39.000 +09', 'Manufacturer': '현대', 'Price': 2590.0,
         #  'Year': 201705.0, 'Mileage': 67541.0, 'Model': '더 뉴 맥스크루즈', 'Badge': '디젤 2.2 4WD', '조회수': '1042', '찜수': '3'},
     ]
+
+
+def get_separated_by_status():
+    return {
+        'newer':
+            [{'Id': '99999999', 'ModifiedDate': '2019-10-09 23:57:38.000 +09', 'Manufacturer': '현대', 'Price': 1950.0,
+              'Year': 201402.0, 'Mileage': 79786.0, 'Model': '맥스크루즈', 'Badge': '디젤(e-VGT) 2.2 4WD 익스클루시브', '조회수': '200',
+              '찜수': '2'}],
+        'leave':
+            [{'Id': '25109309', 'ModifiedDate': '2019-10-09 23:53:59.000 +09', 'Manufacturer': '현대', 'Price': 1750.0,
+              'Year': 201302.0, 'Mileage': 33488.0, 'Model': '싼타페 DM', 'Badge': '디젤(e-VGT) 2.0 2WD 프리미엄', '조회수': '933',
+              '찜수': '9'},
+             {'Id': '25479305', 'ModifiedDate': '2019-10-09 23:56:59.000 +09', 'Manufacturer': '현대', 'Price': 2270.0,
+              'Year': 201708.0, 'Mileage': 8098.0, 'Model': '코나', 'Badge': '1.6 터보 4WD', '조회수': '720', '찜수': '6'}],
+        'deleted':
+            [{'Id': '25377212', 'ModifiedDate': '2019-10-01 20:18:39.000 +09', 'Manufacturer': '현대', 'Price': 2590.0,
+              'Year': 201705.0, 'Mileage': 67541.0, 'Model': '더 뉴 맥스크루즈', 'Badge': '디젤 2.2 4WD', '조회수': '1042',
+              '찜수': '3'},
+             {'Id': '25167131', 'ModifiedDate': '2019-10-09 23:57:38.000 +09', 'Manufacturer': '현대', 'Price': 1950.0,
+              'Year': 201402.0, 'Mileage': 79786.0, 'Model': '맥스크루즈', 'Badge': '디젤(e-VGT) 2.2 4WD 익스클루시브', '조회수': '200',
+              '찜수': '2'}]
+    }
